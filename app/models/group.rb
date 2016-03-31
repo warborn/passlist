2c039eb9be9eefa4b7c68f30dd20dd329964f49d
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-03-31 23:43:07 +0000

    Refactor method for adding a course class to a group

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,4 +12,40 @@ class Group < ActiveRecord::Base
   #   CourseClass.where(classday_id: classdays.map(&:id))
   # end
 
+  def add_class_to_day(new_class)
+    english_day = new_class.date.strftime("%A")
+    spanish_day = day_english_to_spanish(english_day)
+
+    classdays.each do |classday|
+      if classday.day == spanish_day
+        classday.course_classes << new_class
+      end
+    end
+  end
+
+  private
+
+    # def day_spanish_to_english(spanish_day)
+    #   english_day = case spanish_day.downcase
+    #     when 'lunes' then 'Monday'
+    #     when 'martes' then 'Tuesday'
+    #     when 'miercoles' then 'Wednesday'
+    #     when 'jueves' then 'Thursday'
+    #     when 'viernes' then 'Friday'
+    #     when 'sabado' then 'Saturday'
+    #     when 'domingo' then 'Sunday'
+    #   end
+    # end
+
+    def day_english_to_spanish(english_day)
+      spanish_day = case english_day.downcase
+      when 'monday' then 'Lunes'
+      when 'tuesday' then 'Martes'
+      when 'wednesday' then 'Miercoles'
+      when 'thursday' then 'Jueves'
+      when 'friday' then 'Viernes'
+      when 'saturday' then 'Sabado'
+      when 'sunday' then 'Domingo'
+      end
+    end
 end
