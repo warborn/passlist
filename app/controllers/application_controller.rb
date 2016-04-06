0d2e86dcd77b2f88c4caec359fe293dba076e646
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-04-05 21:55:52 -0500

    Refactor serialization helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::API
 
   def serialize(object)
-    if object.is_a?(Array)
-      object.map do |item|
-        ActiveModel::SerializableResource.new(item)
-      end
-    else
-      ActiveModel::SerializableResource.new(object)
-    end
+    ActiveModel::SerializableResource.new(object)
   end
 
 end
