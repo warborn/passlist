aff0e2c24a4d32d76b9a40b6bd6b2dad447b1a88
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-05-20 05:31:18 +0000

    Configure devise_token_auth to not change the access-token after each request

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -5,6 +5,7 @@ DeviseTokenAuth.setup do |config|
   # each request.
   # config.change_headers_on_each_request = true
    config.default_confirm_success_url = "confirmed"
+   change_headers_on_each_request = false
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
